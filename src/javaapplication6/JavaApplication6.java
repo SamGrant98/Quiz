2661f52bbdfbb1f8bb149f7766ca0d52b6e51617
1515515 <1515515@ACCALC212-16.ad.dundeeandangus.ac.uk>
2016-09-07 09:32:22 +0100

    New Commit

---
+++ src/javaapplication6/JavaApplication6.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication6;
+
+import java.util.Scanner;
+ /* @author 1515515
+ */
+public class JavaApplication6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       String Answer1, Answer2, Answer3, Answer4, Answer5;
+       
+       System.out.println("?");
+        Answer1 = input.nextLine();
+        
+       System.out.println("?");
+        Answer2 = input.nextLine();
+        
+        if Answer1 = "A"; 
+    }
+    
+}
